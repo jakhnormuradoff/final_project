4d1702688a8663f67733408e69b9630116db3662
Jakhongir Normurodov <jakhongir.normuradoff@gmail.com>
2025-12-30 23:05:13 +0500

    d, y added

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/jakhnormuradoff/final_project/pkg/api"
 )
 
-
 func StartServer(webDir string, port string) error {
-	fmt.Println("Запускаем сервер")
+	fmt.Println("Запускаем сервеsр")
+	api.Init()
 	http.Handle("/", http.FileServer(http.Dir(webDir)))
 	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
